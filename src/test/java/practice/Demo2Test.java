d90872f2dc25b5f198f78f5940d9c55052af7f09
mrudu <mrudu@LAPTOP-PF1SH7SD>
2022-01-09 05:34:26 +0530

    new commit on 9 th jan 2022

--- src/test/java/practice/Demo2Test.java
+++ src/test/java/practice/Demo2Test.java
@@ -7,6 +7,7 @@ public class Demo2Test {
 	public void demo2Test() {
 		System.out.println("reg-suite");
 		System.out.println("Regression running");
+		System.out.println("added into github repo- new commit on 9th Jan");
 	}
 
 }

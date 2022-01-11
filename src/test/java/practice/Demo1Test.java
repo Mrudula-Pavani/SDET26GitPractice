c9d33cd18e9f2a3b18122d120fd929e6b13395b4
mrudu <mrudu@LAPTOP-PF1SH7SD>
2022-01-11 14:52:12 +0530

    commit1

--- src/test/java/practice/Demo1Test.java
+++ src/test/java/practice/Demo1Test.java
@@ -5,10 +5,7 @@ import org.testng.annotations.Test;
 public class Demo1Test {
 	@Test
 	public void demo1Test() {
-		System.out.println("Demo1 executed");
-		System.out.println("Demo1 cool");
-		System.out.println("new change in the project, try to push ");
-		System.out.println("new change in the project on 9th Jan, try to push ");
+		System.out.println("deleted old data");
 	}
 
 }
